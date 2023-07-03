3d122507af950638862307213301e90cf9b0c45c
Vladyslav Buhai <vladbugaj30@gmail.com>
2023-07-03 22:52:08 +0300

    set up validation pipe and global guard

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
-import { NestFactory } from '@nestjs/core';
+import { NestFactory, Reflector } from '@nestjs/core';
 import { ApplicationModule } from './modules/Application';
+import { Logger, ValidationPipe } from '@nestjs/common';
+import { JwtAuthGuard } from './guards/JwtGuard';
 
 async function bootstrap() {
   const app = await NestFactory.create(ApplicationModule);
-  await app.listen(3000);
+  const logger = new Logger('NestApplication');
+  const reflector = app.get(Reflector);
+
+  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalGuards(new JwtAuthGuard(reflector));
+
+  logger.log(`Server started on the ${ApplicationModule.port} port.`);
+  await app.listen(ApplicationModule.port || 3000);
 }
 bootstrap();
