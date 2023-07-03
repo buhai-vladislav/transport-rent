19e371e7b0def8fce47c114b97ddd8dc102c5104
Vladyslav Buhai <vladbugaj30@gmail.com>
2023-07-03 20:58:23 +0300

    configure rent module by providers and controller

--- src/modules/Rent.ts
+++ src/modules/Rent.ts
@@ -1,10 +1,18 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
-import { Rent, RentSchema } from 'src/db/schemas/Rent';
+import { RentController } from '../controllers/Rent';
+import { Rent, RentSchema } from '../db/schemas/Rent';
+import { Transport, TransportSchema } from '../db/schemas/Transport';
+import { RentService } from '../services/Rent';
 
 @Module({
   imports: [
-    MongooseModule.forFeature([{ name: Rent.name, schema: RentSchema }]),
+    MongooseModule.forFeature([
+      { name: Rent.name, schema: RentSchema },
+      { name: Transport.name, schema: TransportSchema },
+    ]),
   ],
+  controllers: [RentController],
+  providers: [RentService],
 })
 export class RentModule {}
