6b643a1a5c3f084eac7eb6e6c4c0998a6ce26e3e
Vladyslav Buhai <vladbugaj30@gmail.com>
2023-07-04 23:16:07 +0300

    configured swagger options and module

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,8 @@ import { NestFactory, Reflector } from '@nestjs/core';
 import { ApplicationModule } from './modules/Application';
 import { Logger, ValidationPipe } from '@nestjs/common';
 import { JwtAuthGuard } from './guards/JwtGuard';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { JWT_BEARER_SWAGGER_AUTH_NAME } from './utils/constants';
 
 async function bootstrap() {
   const app = await NestFactory.create(ApplicationModule);
@@ -10,6 +12,26 @@ async function bootstrap() {
 
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalGuards(new JwtAuthGuard(reflector));
+
+  const config = new DocumentBuilder()
+    .setTitle('Transport Rent project API')
+    .setDescription('The Transport Rent project API endpoints documentation.')
+    .setVersion('1.0')
+    .addBearerAuth(
+      {
+        type: 'http',
+        scheme: 'bearer',
+        bearerFormat: 'JWT',
+        name: 'JWT',
+        description: 'Enter JWT token',
+        in: 'header',
+      },
+      JWT_BEARER_SWAGGER_AUTH_NAME, // This name here is important for matching up with @ApiBearerAuth() in the controller!
+    )
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
 
   logger.log(`Server started on the ${ApplicationModule.port} port.`);
   await app.listen(ApplicationModule.port || 3000);
