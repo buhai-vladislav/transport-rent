5a09796962e9869858ec9ec182a468f62d6f6faa
Vladyslav Buhai <vladbugaj30@gmail.com>
2023-07-11 20:24:39 +0300

    add types for price and power ranges

--- src/types/TransportWhere.ts
+++ src/types/TransportWhere.ts
@@ -2,10 +2,10 @@ import { LicenceType, TransportType } from '../db/schemas/Transport';
 import { Where } from './Where';
 
 export class TransportWhere extends Where {
-  priceRange?: [number, number];
+  priceRange?: [number, number] | [string, string];
   maxSpeed?: number;
   color?: string;
-  powerRange?: [number, number];
+  powerRange?: [number, number] | [string, string];
   type?: TransportType;
   licenceType?: LicenceType;
 }
