b8155319478c1855c522f0f280db243edf0e62ab
Vladyslav Buhai <vladbugaj30@gmail.com>
2023-07-12 20:35:14 +0300

    add links by roles and logout handler

--- src/client/src/components/Header/Header.tsx
+++ src/client/src/components/Header/Header.tsx
@@ -1,15 +1,49 @@
-import { Avatar, Button, Navbar, Popover, Text } from '@nextui-org/react';
+import {
+  Avatar,
+  Button,
+  Loading,
+  Navbar,
+  Popover,
+  Text,
+} from '@nextui-org/react';
 import { Link, useNavigate } from 'react-router-dom';
 import { Logo } from '../../icons';
 import { useAppSelector } from '../../store/hooks/hooks';
+import { useLogoutMutation } from '../../store/api/main.api';
+import { useCallback } from 'react';
+import { IMutation } from '../../types/RTK';
+import { IAffectedResult, IResponse } from '../../types/Response';
+import { useErrorToast } from '../../hooks/useErrorToast';
+import { HttpStatus } from '../../types/HttpStatus';
 
 export const Header = () => {
   const navigate = useNavigate();
   const { user } = useAppSelector((state) => state.user);
+  const [logout, { error, isLoading }] = useLogoutMutation();
 
-  const logoutHandler = () => {
-    navigate('/login');
-  };
+  const logoutHandler = useCallback(async () => {
+    const token = localStorage.getItem('refreshToken');
+
+    if (token) {
+      const response: IMutation<IResponse<IAffectedResult>> = await logout(
+        token,
+      );
+
+      if (response.data?.data?.isAffected) {
+        localStorage.clear();
+        navigate('/signin');
+      }
+    }
+  }, []);
+
+  useErrorToast(
+    error,
+    [
+      { status: HttpStatus.BAD_REQUEST },
+      { status: HttpStatus.INTERNAL_SERVER_ERROR },
+    ],
+    { position: 'bottom-center', type: 'error' },
+  );
 
   return (
     <Navbar isBordered variant="sticky">
@@ -31,11 +65,25 @@ export const Header = () => {
           </>
         ) : (
           <>
+            {user.role === 'USER' && (
+              <Link to="rent-list">
+                <Text h5 css={{ marginBottom: '0' }}>
+                  Rent-List
+                </Text>
+              </Link>
+            )}
             <Link to="transports">
               <Text h5 css={{ marginBottom: '0' }}>
                 Transports
               </Text>
             </Link>
+            {user.role === 'ADMIN' && (
+              <Link to="transports/create">
+                <Text h5 css={{ marginBottom: '0' }}>
+                  Add new
+                </Text>
+              </Link>
+            )}
             <Popover placement="left">
               <Popover.Trigger>
                 <Avatar
@@ -47,8 +95,8 @@ export const Header = () => {
                 />
               </Popover.Trigger>
               <Popover.Content>
-                <Button size="sm" onPress={logoutHandler}>
-                  Log out
+                <Button size="sm" onPress={logoutHandler} disabled={isLoading}>
+                  {isLoading ? <Loading size="sm" /> : 'Logout'}
                 </Button>
               </Popover.Content>
             </Popover>
