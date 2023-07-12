b0fc628f0d297ad5b95fd00fa09b3513f3780081
Vladyslav Buhai <vladbugaj30@gmail.com>
2023-07-12 20:31:30 +0300

    add role guard for protected route

--- src/client/src/shared/ProtectedRoute/ProtectedRoute.props.ts
+++ src/client/src/shared/ProtectedRoute/ProtectedRoute.props.ts
@@ -3,6 +3,7 @@ import { ReactElement } from 'react';
 interface IPrivateRouteProps {
   children?: ReactElement;
   redirectPath?: string;
+  roles?: string[];
 }
 
 export type { IPrivateRouteProps };
--- src/client/src/shared/ProtectedRoute/ProtectedRoute.tsx
+++ src/client/src/shared/ProtectedRoute/ProtectedRoute.tsx
@@ -1,16 +1,26 @@
 import { FC } from 'react';
-import { Navigate, Outlet } from 'react-router-dom';
+import { Navigate, Outlet, useLocation } from 'react-router-dom';
 import { useAppSelector } from '../../store/hooks/hooks';
 import { IPrivateRouteProps } from './ProtectedRoute.props';
 
 export const ProtectedRoute: FC<IPrivateRouteProps> = ({
   children,
   redirectPath = '/',
+  roles,
 }) => {
   const { user } = useAppSelector((state) => state.user);
+  const { pathname } = useLocation();
 
   if (!user) {
     return <Navigate to={redirectPath} replace />;
+  }
+
+  if (user && roles?.length) {
+    if (roles.includes(user.role)) {
+      return children ?? <Outlet />;
+    } else {
+      return <Navigate to={redirectPath} replace />;
+    }
   }
 
   return children ?? <Outlet />;
